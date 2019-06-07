2feda34636d33ac18c63a3496478c8974aa287aa
theroi <40249824+theroi@users.noreply.github.com>
2019-06-07 12:01:51 +0200

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,9 @@
 public class HelloWorld {
-//JE COMMENTE ICI
+
 	public static void main(String[] args) {
 		// System.out.println permet d'afficher un message sur la console
 		System.out.println("Hello World !!!");
 		System.out.println("Bientôt un vrai programme");
+		System.out.println("Mise-à-jour sur GitHub");
 	}
 }
