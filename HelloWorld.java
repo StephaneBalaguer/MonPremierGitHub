0a17daae0d2a9a3e4586713e23be4d2293081ff4
Stéphane Balaguer <st.balaguer@gmail.com>
2019-06-07 12:01:08 +0200

    moddification de HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,8 @@
 public class HelloWorld {
-
+//JE COMMENTE ICI
 	public static void main(String[] args) {
+		// System.out.println permet d'afficher un message sur la console
 		System.out.println("Hello World !!!");
+		System.out.println("Bientôt un vrai programme");
 	}
-
 }
